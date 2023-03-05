770b77bc5773c2096f8a7b0161eafcb3e668fca9
Michelle Vong <mva30@sfu.ca>
2023-03-04 20:53:10 -0800

    Add path to sidebar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/home/Home';
+import NavBar from './components/home/Navbar';
+import Sidebar from './components/home/Sidebar';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="home" element={<Home />}></Route>
+        <Route path="navbar" element={<NavBar />}></Route>
+        <Route path="sidebar" element={<Sidebar />}></Route>
       </Routes>
     </BrowserRouter>
   );
