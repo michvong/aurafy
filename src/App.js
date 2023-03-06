2a750e727ba0ee17e8d344d337dcf3efbfc2cf68
Michelle Vong <mva30@sfu.ca>
2023-03-06 00:31:54 -0800

    Add link to playlist

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './components/home/Home';
+import Home from './components/home/Welcome';
 import NavBar from './components/home/Navbar';
 import Sidebar from './components/home/Sidebar';
+import Playlist from './components/home/Playlist';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="home" element={<Home />}></Route>
         <Route path="navbar" element={<NavBar />}></Route>
         <Route path="sidebar" element={<Sidebar />}></Route>
+        <Route path="playlist" element={<Playlist />}></Route>
       </Routes>
     </BrowserRouter>
   );
