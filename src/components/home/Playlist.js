2bbd5e90970df471b2718711a8db11b8cc85101c
Michelle Vong <mva30@sfu.ca>
2023-03-06 00:17:38 -0800

    Add music icon when playlist image doesn't exist

--- src/components/home/Playlist.js
+++ src/components/home/Playlist.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Music from '../../assets/music.svg';
 
 export default function Playlist() {
   const [playlistImage, setPlaylistImage] = useState();
@@ -9,11 +10,17 @@ export default function Playlist() {
     <div>
       <div class="flex flex-col items-center justify-items-center content-center block px-1 py-2 w-44 bg-stone-700 border border-gray-200 rounded-lg shadow">
         <div class="flex justify-center content-center mt-2">
-          <img
-            src={playlistImage || 'https://via.placeholder.com/300'}
-            class="rounded-lg max-w-[80%] max-h-[80%] bg-gray-300"
-            alt="Placeholder"
-          />
+          {playlistImage ? (
+            <img
+              src={playlistImage}
+              class="rounded-lg max-w-36 max-h-36 bg-gray-400"
+              alt="Playlist image"
+            />
+          ) : (
+            <div class="w-36 h-36 bg-gray-300 flex justify-center items-center">
+              <img src={Music} alt="Music icon" style={{ width: '50%', height: '50%' }} />
+            </div>
+          )}
         </div>
 
         <h5 class="truncate mt-2 text-base font-bold tracking-tight text-white">{playlistName}</h5>
