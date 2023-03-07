77b590fb7e18a4d01affa6935c93c86ca87fb1d6
Michelle Vong <mva30@sfu.ca>
2023-03-06 23:56:14 -0800

    Fix dropdown menu

--- src/components/home/Navbar.js
+++ src/components/home/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <nav class="bg-stone-900 border-gray-200 px-2 sm:px-4 py-2.5 rounded-md">
@@ -15,72 +17,69 @@ export default function NavBar() {
               aurafy
             </span>
           </div>
-          <div class="flex items-center md:order-2">
+          <div className="relative">
             <button
-              type="button"
-              class="flex mr-3 text-sm bg-white rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300"
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="user-dropdown"
-              data-dropdown-placement="bottom"
+              onClick={() => setIsOpen(!isOpen)}
+              className="flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out"
             >
-              <span class="sr-only">Open user menu</span>
-              <div class="w-8 h-8 rounded-full"></div>
-            </button>
-            <div
-              class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
-              id="user-dropdown"
-            >
-              <div class="px-4 py-3">
-                <span class="block text-sm text-gray-900">Bonnie Green</span>
-                <span class="block text-sm font-medium text-gray-500 truncate">
-                  name@flowbite.com
-                </span>
-              </div>
-              <ul class="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                    Earnings
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                    Sign out
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-              aria-expanded="false"
-            >
-              <span class="sr-only">Open main menu</span>
+              <img
+                class="m-1 h-8 w-8 rounded-full"
+                src="https://via.placeholder.com/50"
+                alt="Profile Picture"
+              />
+              <p class="m-1.5 font-medium text-white">Bonnie Green</p>
               <svg
-                class="w-6 h-6"
+                class="w-4 h-4 mr-1"
                 aria-hidden="true"
-                fill="currentColor"
+                fill="white"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                   clip-rule="evenodd"
                 ></path>
               </svg>
             </button>
+
+            <div
+              id="dropdownAvatar"
+              className={`z-10 ${
+                isOpen ? '' : 'hidden'
+              } absolute mt-2 bg-stone-900 divide-y divide-gray-600 rounded-lg shadow w-44`}
+              style={{ top: 'calc(100% + 0.5rem)', left: '0' }}
+            >
+              <div class="px-4 py-3 text-sm text-white">
+                <div>Bonnie Green</div>
+                <div class="font-medium truncate">name@flowbite.com</div>
+              </div>
+              <ul class="py-2 text-sm text-gray-200" aria-labelledby="dropdownUserAvatarButton">
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-600 hover:text-white">
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-600 hover:text-white">
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-600 hover:text-white">
+                    Earnings
+                  </a>
+                </li>
+              </ul>
+              <div class="py-2">
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
+                >
+                  Sign out
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
