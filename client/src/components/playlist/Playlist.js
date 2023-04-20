7a3ad048ebfac7be6c220b2b2fb965338e842e34
Michelle Vong <mva30@sfu.ca>
2023-04-19 18:01:20 -0700

    Add checking to see if playlist is empty

--- client/src/components/playlist/Playlist.js
+++ client/src/components/playlist/Playlist.js
@@ -59,47 +59,55 @@ export default function Playlist({ playlistId }) {
             </tr>
           </thead>
           <tbody>
-            {playlist.tracks.items.map((item) => (
-              <tr key={item.track.id} class="group bg-stone-800 hover:bg-stone-700">
-                <td class="px-4 py-2">
-                  <div class="relative">
-                    <span class="group-hover:text-transparent">{trackNumberCounter++}</span>
-                    <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="#ffffff"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon points="5 3 19 12 5 21 5 3"></polygon>
-                      </svg>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-4 py-2 flex items-center">
-                  <img
-                    src={item.track.album.images[0].url}
-                    alt={`${item.track.album.name} album cover`}
-                    class="mr-4 w-10 h-10"
-                  />
-                  <div class="flex flex-col">
-                    <div class="font-semibold text-white">{item.track.name}</div>
-                    <div class="text-stone-300">
-                      {item.track.artists.map((artist) => artist.name).join(', ')}
-                    </div>
-                  </div>
-                </td>
-                <td class="px-4 py-2 text-left">_ _ _ _ _</td>
-                <td class="px-4 py-2 text-right">
-                  {msToMinutesAndSeconds(item.track.duration_ms)}
+            {playlist.tracks.items.length === 0 ? (
+              <tr>
+                <td class="px-4 py-2" colspan="4">
+                  Playlist is empty.
                 </td>
               </tr>
-            ))}
+            ) : (
+              playlist.tracks.items.map((item) => (
+                <tr key={item.track.id} class="group bg-stone-800 hover:bg-stone-700">
+                  <td class="px-4 py-2">
+                    <div class="relative">
+                      <span class="group-hover:text-transparent">{trackNumberCounter++}</span>
+                      <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="#ffffff"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <polygon points="5 3 19 12 5 21 5 3"></polygon>
+                        </svg>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="px-4 py-2 flex items-center">
+                    <img
+                      src={item.track.album.images[0].url}
+                      alt={`${item.track.album.name} album cover`}
+                      class="mr-4 w-10 h-10"
+                    />
+                    <div class="flex flex-col">
+                      <div class="font-semibold text-white">{item.track.name}</div>
+                      <div class="text-stone-300">
+                        {item.track.artists.map((artist) => artist.name).join(', ')}
+                      </div>
+                    </div>
+                  </td>
+                  <td class="px-4 py-2 text-left">_ _ _ _ _</td>
+                  <td class="px-4 py-2 text-right">
+                    {msToMinutesAndSeconds(item.track.duration_ms)}
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
