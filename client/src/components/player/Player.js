86b5b51b9973d0fd3ad6c466e6f0c63df4858a1d
Michelle Vong <mva30@sfu.ca>
2023-05-02 19:13:55 -0700

    Disable player when initial song hasn't played yet

--- client/src/components/player/Player.js
+++ client/src/components/player/Player.js
@@ -1,14 +1,21 @@
 import React, { useEffect } from 'react';
+import { usePlaybackState } from 'react-spotify-web-playback-sdk';
 import SongDetails from './SongDetails';
 import Controller from './Controller';
 import Volume from './Volume';
 
 export default function Player() {
+  const playbackState = usePlaybackState({ interval: true });
+
   return (
-    <div class="bottom-0 left-0 right-0 bg-stone-800 border-t border-stone-700 px-3 py-1 flex items-center justify-between">
-      <SongDetails />
-      <Controller />
-      <Volume />
+    <div class="relative">
+      <div class="bottom-0 left-0 right-0 bg-stone-800 border-t border-stone-700 px-3 py-1 flex items-center justify-between">
+        <SongDetails />
+        <Controller />
+        <Volume />
+      </div>
+
+      {playbackState === null ? <div class="absolute inset-0 bg-stone-900 opacity-70"></div> : null}
     </div>
   );
 }
