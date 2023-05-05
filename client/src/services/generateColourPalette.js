6aeb463e71ccea4811a6bde3ba138231793c1cf1
Michelle Vong <mva30@sfu.ca>
2023-05-04 17:46:37 -0700

    Add method for colour palette generation

---
+++ client/src/services/generateColourPalette.js
@@ -0,0 +1,23 @@
+import chroma from 'chroma-js';
+
+const generateColourPalette = (danceability, energy, valence, tempo, acousticness) => {
+  const baseColour = '#4285f4';
+
+  const colourPalette = chroma
+    .scale([chroma(baseColour).darken(2), baseColour, chroma(baseColour).brighten(2)])
+    .colors(5);
+
+  const modifiedPalette = colourPalette.map((colour) => {
+    const modifiedColour = chroma(colour)
+      .saturate(danceability * 5)
+      .brighten(energy * 2)
+      .set('hsl.h', valence * 360)
+      .luminance(tempo / 200)
+      .alpha(acousticness);
+    return modifiedColour;
+  });
+
+  return modifiedPalette;
+};
+
+export default generateColourPalette;
