92d55aa0aae6c0bc5cbd905889c1f8ad6c4f398d
Michelle Vong <mva30@sfu.ca>
2023-05-06 17:45:25 -0700

    Remove unused routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,6 @@ import { UserInfoProvider } from './contexts/UserInfo';
 import { UserPlaylistsProvider } from './contexts/UserPlaylists';
 
 import Home from './pages';
-import Welcome from './components/home/Welcome';
-import NavBar from './components/generic/Navbar';
-import Sidebar from './components/generic/Sidebar';
-import PlaylistItem from './components/home/PlaylistItem';
-import PlaylistList from './components/home/PlaylistList';
-import Playlist from './components/playlist/Playlist';
-import Player from './components/player/Player';
 import Landing from './components/landing/Landing';
 
 function App() {
@@ -23,13 +16,6 @@ function App() {
             <Route path="/" element={<Landing />}></Route>
             <Route path="home" element={<Home />}></Route>
             <Route path=":playlistId" element={<Home />}></Route>
-            {/* <Route path="welcome" element={<Welcome />}></Route>
-            <Route path="navbar" element={<NavBar />}></Route>
-            <Route path="sidebar" element={<Sidebar />}></Route>
-            <Route path="playlist-list" element={<PlaylistList />}></Route>
-            <Route path="playlist-item" element={<PlaylistItem />}></Route>
-            <Route path="playlist" element={<Playlist />}></Route>
-            <Route path="player" element={<Player />}></Route> */}
           </Routes>
         </BrowserRouter>
       </UserPlaylistsProvider>
